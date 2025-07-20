df2e8829fe925ea9772eadea0b92221a737883db
zerocode74 <youremail@example.com>
2025-07-20 17:48:12 +0700

    TES

--- app.js
+++ app.js
@@ -1,56 +1,26 @@
-const logger = require('koa-logger')
-const responseTime = require('koa-response-time')
-const bodyParser = require('koa-bodyparser')
-const ratelimit = require('koa-ratelimit')
-const Router = require('koa-router')
-const Koa = require('koa')
+const logger = require("koa-logger");
+const responseTime = require("koa-response-time");
+const bodyParser = require("koa-bodyparser");
+const Router = require("koa-router");
+const Koa = require("koa");
 
-const app = new Koa()
+const app = new Koa();
 
-app.use(logger())
-app.use(responseTime())
-app.use(bodyParser())
+app.use(logger());
+app.use(responseTime());
+app.use(bodyParser());
 
-const ratelimitВb = new Map()
+app.use(require("./helpers").helpersApi);
 
-app.use(ratelimit({
-  driver: 'memory',
-  db: ratelimitВb,
-  duration: 1000 * 55,
-  errorMessage: {
-    ok: false,
-    error: {
-      code: 429,
-      message: 'Rate limit exceeded. See "Retry-After"'
-    }
-  },
-  id: (ctx) => ctx.ip,
-  headers: {
-    remaining: 'Rate-Limit-Remaining',
-    reset: 'Rate-Limit-Reset',
-    total: 'Rate-Limit-Total'
-  },
-  max: 20,
-  disableHeader: false,
-  whitelist: (ctx) => {
-    return ctx.query.botToken === process.env.BOT_TOKEN
-  },
-  blacklist: (ctx) => {
-  }
-}))
+const route = new Router();
+const routes = require("./routes");
 
-app.use(require('./helpers').helpersApi)
+// Gunakan route utama
+route.use("/*", routes.routeApi.routes());
 
-const route = new Router()
+app.use(route.routes());
 
-const routes = require('./routes')
-
-route.use('/*', routes.routeApi.routes())
-
-app.use(route.routes())
-
-const port = process.env.PORT || 3000
-
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log('Listening on localhost, port', port)
-})
+  console.log("✅ Listening on http://localhost:" + port);
+});
